06ab6066753bbedb98fcfb77e1bad67a658a28df
mvp-triadiprabowo <mvp.triadiprabowo@gmail.com>
2015-04-28 10:41:54 +0700

    1.1b: Update portfolio template add button to slider

--- sys/tpl/portfolio.tpl.php
+++ sys/tpl/portfolio.tpl.php
@@ -11,20 +11,20 @@
 		<section class="tp-slider-content-inner">
 			<h1><span class="tp-icon fa-cloud"></span> Pandora Hotel</h1>
 			<p>Pandora Hotel is the interactive proposal design to hotel in Jakarta, Indonesia. <br />Name has been changed to privacy purpose.</p>
+			<a href="javascript:void(0)" class="tp-button"><span class="tp-icon fa-link"></span> View</a> 
 		</section>
 	</section>
 
 	<!-- Slider Content -->
 	<section class="tp-slider-content">
 		<section class="tp-content-logo">
-			<img src="res/img/css.png" width="200" height="200" title="TP CSS Bootstrap" />
+			<img src="res/img/pandora.png" title="Pandora Hotel Interactive Web Design" />
 		</section>
 
 		<section class="tp-slider-content-inner">
-			<h1><span class="tp-icon fa-cloud"></span> tpCSS Bootstrap</h1>
-			<p>Download latest version of Responsive CSS Bootstrap<br /> Version 1.0 - Initial Build</p>
-			<a href="javascript:void(0)" class="tp-button"><span class="tp-icon fa-download"></span> Download</a> 
-			<a href="javascript:void(0)" class="tp-button tp-btn-alt"><span class="tp-icon fa-book"></span> Docs</a>
+			<h1><span class="tp-icon fa-cloud"></span> Pandora Hotel</h1>
+			<p>Pandora Hotel is the interactive proposal design to hotel in Jakarta, Indonesia. <br />Name has been changed to privacy purpose.</p>
+			<a href="javascript:void(0)" class="tp-button"><span class="tp-icon fa-link"></span> View</a> 
 		</section>
 	</section>
 

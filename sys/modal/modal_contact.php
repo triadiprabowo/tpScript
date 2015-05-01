e117b03b4026b6f24596dd46734e777877aef4f1
triadiprabowo <triadiprabowo@gmail.com>
2015-05-01 21:48:41 +0700

    Updated => contact form fixed

--- sys/modal/modal_contact.php
+++ sys/modal/modal_contact.php
@@ -1,10 +1,29 @@
 <section class="tp-modal-wrapper medium">
 	<!-- Add close icon modal -->
-	<div class="tp-modal-closeicon" ng-click="closeThisDialog()"><span class="tp-icon fa-times"></span></div>
+	<div class="tp-modal-closeicon hvr-buzz-out" ng-click="closeThisDialog()"><span class="tp-icon fa-times"></span></div>
 
 	<section class="tp-modal-inner">
 		<section class="tp-modal-content">
 			<h1>Contact Me!</h1>
+			
+			<span class="tp-modal-separator"></span>
+
+			<!-- Contact Form -->
+			<p><span class="tp-icon fa-user"></span> Full Name</p>
+			<input type="text" class="tp-txtbox" placeholder="Please input your full name" />
+
+			<p><span class="tp-icon fa-envelope-o"></span>  Email</p>
+			<input type="text" class="tp-txtbox" placeholder="Please input your valid email address" />
+
+			<p><span class="tp-icon fa-tags"></span>  Subject</p>
+			<input type="text" class="tp-txtbox" placeholder="What this is about?" />
+
+			<p><span class="tp-icon fa-inbox"></span>  Message</p>
+			<textarea class="tp-txtbox" placeholder="What do you want to say?"></textarea>
+
+			<div class="tp-modal-submit right">
+				<a href="javascript:void(0)" class="tp-button"><span class="tp-icon fa-paper-plane"></span> Send</a>
+			</div>
 		</section>
 	</section>
 </section>

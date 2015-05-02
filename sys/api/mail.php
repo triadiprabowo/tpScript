8608445255707cab3746a7a2f34e8ff6c3b26847
triadiprabowo <triadiprabowo@gmail.com>
2015-05-02 16:23:27 +0700

    Major update: add API and success modal dialog

---
+++ sys/api/mail.php
@@ -0,0 +1,25 @@
+<?php
+$form = mysql_escape_string($_POST['form']);
+
+if($form == "contact") {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = 'Contact Inquiry - '.$_POST['subject'];	
+	$mail_to = 'me@triadiprabowo.com.com';
+	$message = $_POST['message'];	
+	
+	$body_message = '<html><body><font size="+3" style="color: #5ECCFF;"><b>Contact Inquiry</b></font>'."\r\n";
+	$body_message .= "<hr />\r\n";
+	$body_message .= '<font style="line-height: 2em;"><b>Sender Name:</b> '.$name."<br />";
+	$body_message .= '<b>Sender Email:</b> '.$email."</font><br /><br />";
+	$body_message .= '<font size="+1"><b>Message:</b></font> '.'<br /><font style="margin: .5em 0; line-height: 2em;"><i>'.$message.'</i></font>';
+	$body_message .= '<br /><br />Send from triadiprabowo.com - contact form</body></html>';
+	
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: '.$email."\r\n";
+	$headers .= 'Reply-To: '.$email."\r\n";
+	
+	mail($mail_to, $subject, $body_message, $headers);
+}
+?>
---
+++ sys/modal/success.php
@@ -0,0 +1,24 @@
+<?php 
+if($_GET['res'] == 'contact') { ?>
+	<section class="tp-modal-wrapper medium">
+		<!-- Add close icon modal -->
+		<div class="tp-modal-closeicon hvr-buzz-out" ng-click="closeThisDialog()"><span class="tp-icon fa-times"></span></div>
+
+		<section class="tp-modal-inner">
+			<section class="tp-modal-content">
+				<h1><span class="tp-icon fa-check-circle"></span> Contact Me!</h1>
+				
+				<span class="tp-modal-separator"></span>
+
+				<!-- Contact Form -->
+				<p style="text-align: center;">Thank you for contacting me and I will return to you as soon as possible.</p>
+
+				<div class="tp-modal-submit right">
+					<a href="javascript:void(0)" class="tp-button" ng-click="closeThisDialog()"><span class="tp-icon fa-check"></span> OK</a>
+				</div>
+			</section>
+		</section>
+	</section>
+<?php
+} // End if res == contact
+?>

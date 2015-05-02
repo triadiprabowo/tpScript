804d1dec87c0b4e2281d59645affcf438b477f49
triadiprabowo <triadiprabowo@gmail.com>
2015-05-02 17:26:30 +0700

    1.1b Major: updating contact modal

--- sys/modal/modal_contact.php
+++ sys/modal/modal_contact.php
@@ -10,19 +10,19 @@
 
 			<!-- Contact Form -->
 			<p><span class="tp-icon fa-user"></span> Full Name</p>
-			<input type="text" class="tp-txtbox nobg" placeholder="Your full name" />
+			<input type="text" class="tp-txtbox nobg" ng-model="fullname" name="name" placeholder="Your full name" />
 
 			<p><span class="tp-icon fa-envelope-o"></span>  Email</p>
-			<input type="text" class="tp-txtbox nobg" placeholder="Your email address (e.g. email@email.com)" />
+			<input type="text" class="tp-txtbox nobg" ng-model="email" name="email" placeholder="Your email address (e.g. email@email.com)" />
 
 			<p><span class="tp-icon fa-tags"></span>  Subject</p>
-			<input type="text" class="tp-txtbox nobg" placeholder="What this is about?" />
+			<input type="text" class="tp-txtbox nobg" ng-model="subject" name="subject" placeholder="What this is about?" />
 
 			<p><span class="tp-icon fa-inbox"></span>  Message</p>
-			<textarea class="tp-txtarea nobg" placeholder="What do you want to say?"></textarea>
+			<textarea class="tp-txtarea nobg" ng-model="message" name="message" placeholder="What do you want to say?"></textarea>
 
 			<div class="tp-modal-submit right">
-				<a href="javascript:void(0)" class="tp-button"><span class="tp-icon fa-paper-plane"></span> Send</a>
+				<a href="javascript:void(0)" class="tp-button" ng-click="submitContact()"><span class="tp-icon fa-paper-plane"></span> Send</a>
 			</div>
 		</section>
 	</section>

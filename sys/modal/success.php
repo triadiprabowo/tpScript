83ab2b7eaa816eca0378b9daaa1ca52476385961
triadiprabowo <triadiprabowo@gmail.com>
2015-05-02 17:26:52 +0700

    1.1b Major: add refreshClosing() function

--- sys/modal/success.php
+++ sys/modal/success.php
@@ -2,11 +2,11 @@
 if($_GET['res'] == 'contact') { ?>
 	<section class="tp-modal-wrapper medium">
 		<!-- Add close icon modal -->
-		<div class="tp-modal-closeicon hvr-buzz-out" ng-click="closeThisDialog()"><span class="tp-icon fa-times"></span></div>
+		<div class="tp-modal-closeicon hvr-buzz-out" ng-click="refreshClose()"><span class="tp-icon fa-times"></span></div>
 
 		<section class="tp-modal-inner">
 			<section class="tp-modal-content">
-				<h1><span class="tp-icon fa-check-circle"></span> Contact Me!</h1>
+				<h1><span class="tp-icon fa-check-circle"></span> Sucess</h1>
 				
 				<span class="tp-modal-separator"></span>
 
@@ -14,7 +14,7 @@ if($_GET['res'] == 'contact') { ?>
 				<p style="text-align: center;">Thank you for contacting me and I will return to you as soon as possible.</p>
 
 				<div class="tp-modal-submit right">
-					<a href="javascript:void(0)" class="tp-button" ng-click="closeThisDialog()"><span class="tp-icon fa-check"></span> OK</a>
+					<a href="javascript:void(0)" class="tp-button" ng-click="refreshClose()"><span class="tp-icon fa-check"></span> OK</a>
 				</div>
 			</section>
 		</section>

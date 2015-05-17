5fd30c9cacbc4e1c63c964b4b3ba24cbe166d601
triadiprabowo <triadiprabowo@gmail.com>
2015-05-17 18:34:32 +0700

    Resolve missing files

---
+++ sys/tpl/resume.tpl.php
@@ -0,0 +1,145 @@
+<?php 	
+	include('../../init.script.php'); 
+	include('../../header.php');
+?>
+
+<!-- Contents -->
+<section class="tp-content-wrapper lowindex" id="aboutme">
+	<section class="tp-content-inner">
+		<div class="floatright">
+			<img src="res/img/dp2.jpg" width="147" height="220" class="imgnormal" />
+		</div>
+
+		<div class="floatleft">
+			<h2 class="titlecolor">Resume</h2>
+
+			<p>	Triadi Prabowo<br />
+				me@triadiprabowo.com<br />
+				Jl. Pulo Sirih Utara Raya Blok DA No. 89<br />
+				Bekasi, West Java, Indonesia
+			</p>
+		</div>
+
+		<div id="clearfloat"></div>
+
+		<h4 class="txtheading">Education</h4>
+		<p class="txtsubheading">Binus University<br />
+			Jakarta, Indonesia (2011 - 2015)			
+		</p>
+		<p class="txtnormal">
+			Bachelor of Computer Science (BSc) or Strata-1 (S1) Computer, major in E-Business of School Information System.
+		</p>
+
+		<!-- N -->
+		<p class="txtsubheading">Inholland University<br />
+			Amsterdam, The Netherlands (2013)			
+		</p>
+		<p class="txtnormal">
+			Student exchange spring semester program. Class of Mathematics/IT, courses: JavaScript, ITIL Basic Service Management, Data Modelling in Access, Project Application Excel
+		</p>
+
+		<!-- Working Experience -->
+
+		<h4 class="txtheading">Working Experience</h4>
+		<p class="txtsubheading">Dinas Perhubungan (Dishub) - UPT Terminal Rawamangun<br />
+			Network Technician<br />
+			Jakarta, Indonesia (2009)			
+		</p>
+		<p class="txtnormal">
+			Implement & setting up wireless network connection to all Desktop PC, includes: sharing files, devices or peripherals in Windows OS
+		</p>
+
+		<!-- N -->
+		<p class="txtsubheading">Fixy-Fact.Com<br />
+			Founder & Front End Developer<br />
+			Jakarta, Indonesia (2009 - 2012)			
+		</p>
+		<p class="txtnormal">
+			Develop new features in forum using phpBB3 modules, maintain user interface and experience to get best functionality or accessibility for forum members
+		</p>
+
+		<!-- N -->
+		<p class="txtsubheading">PT. Adi Pertiwi<br />
+			Intern &mdash; Web Developer<br />
+			Jakarta, Indonesia (Sep 2014 - Jan 2015)			
+		</p>
+		<p class="txtnormal">
+			<li>Create the company Front-End interface design, responsive and mobile compatibility. Templating, MVC or HTTP request with AngularJS</li>
+			<li>Create the company back-end system, e-Procurement module REST API in PHP with CodeIgniter framework</li>
+		</p>
+
+		<!-- N -->
+		<p class="txtsubheading">PT. Multi Visi Paragon<br />
+			Front End Developer<br />
+			Jakarta, Indonesia (Apr 2015 - Present)			
+		</p>
+		<p class="txtnormal">
+			<li>Developing new features and maintain usability, accessibility and interaction design of company product (Social Media)</li>
+			<li>Using latest MVC front end technologies with AngularJS, version control with Git and unit testing with Karma</li>
+			<li>Using nodeJs for packages control or modules</li>
+		</p>
+
+		<!-- Leadership Experience -->
+
+		<h4 class="txtheading">Leadership Experience</h4>
+		<p class="txtsubheading">Pemuda Pancasila<br />
+			Branch Leader<br />
+			Jakarta, Indonesia (2009 - 2012)			
+		</p>
+		<p class="txtnormal">
+			<li>Lead of 28 members</li>
+			<li>Create internal events and public social events</li>
+			<li>Achieved subject as team leader with commitment</li>
+		</p>
+
+		<p class="txtsubheading">RanPDF Gaming Network<br />
+			Remote &mdash; Game Assistant & Donate Payment Agent<br />
+			Manilla, Phillipines (2009 - 2012)		
+		</p>
+		<p class="txtnormal">
+			<li>Provided assistance to all players</li>
+			<li>Maintain player relationship and manage customers data record</li>
+			<li>Create social events in game or off game</li>
+			<li>Placement specifically or lead for Indonesia and Malaysia players</li>
+		</p>
+
+		<!-- Skillset -->
+
+		<h4 class="txtheading">Key Skills</h4>
+		<p class="txtnormal">
+			<li>HTML5</li>
+			<li>CSS3 / SASS</li>
+			<li>Javascript / Frameworks (jQuery, AngularJS, websocket.io)</li>
+			<li>Packages Installer with NodeJS</li>
+			<li>Unit testing (Karma)</li>
+			<li>Version control with Git</li>
+			<li>PHP</li>
+			<li>PHP Frameworks (CodeIgniter / Laravel)</li>
+			<li>REST API</li>
+			<li>AJAX/JSON</li>
+			<li>SQL / Access</li>
+			<li>VB.NET / C#</li>
+			<li>System deployment / analyst with UML Diagram</li>
+		</p>
+
+		<h4 class="txtheading">Self Development</h4>
+		<p class="txtnormal">
+			<li>Problem Solver</li>
+			<li>Quick learner</li>
+			<li>Eager to learn something new</li>
+			<li>Process analyst and intelligence</li>
+			<li>Typically getting the job done</li>
+		</p>
+
+		<h4 class="txtheading">Communication</h4>
+		<p class="txtnormal">
+			<li>English &mdash; Professional Working Fluency</li>
+			<li>Bahasa Indonesia &mdash; Native</li>
+		</p>
+	</section>
+</section>
+
+<!-- Include Footer Page -->
+<?php
+	include('../../footer.php');
+?>
